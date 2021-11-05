ca2f0c9f5e18420fe227c42be3a857d7d5eeac64
王健 <1907578518@qq.com>
2021-11-05 10:40:35 +0800

    金沙江管理平台V6更新
    1 增加客服管理

---
+++ 金沙江管理平台V6/files/交互原型/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r,s,t,u],v,_(w,x,y,z,g,A,B,_(),C,[],D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,P,P,Q,R,S,null,T,U,V,W,X,Y,Z,U,ba,bb,_(J,K,L,bc),bd,U,be,bf,_(bg,bh,bi,bj,bk,bj,bl,bj,bm,m,L,_(bn,bo,bp,bo,bq,bo,br,bs)),i,_(j,k,l,m)),bt,_(),bu,_(),bv,_(bw,[])),bx,_(),by,_());}; 
+var b="url",c="交互原型.html",d="generationDate",e=new Date(1636077362057.63),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=1440,l="height",m=0,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="xianshi",s="yincang",t="kehuxinxi",u="OnLoadVariable",v="page",w="packageId",x="cafd65151f7e4d338c77b4d1dfca9fb1",y="type",z="Axure:Page",A="交互原型",B="notes",C="annotations",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="center",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageAlignment",P="near",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="borderVisibility",bb="borderFill",bc=0xFF797979,bd="cornerRadius",be="cornerVisibility",bf="outerShadow",bg="on",bh=false,bi="offsetX",bj=5,bk="offsetY",bl="blurRadius",bm="spread",bn="r",bo=0,bp="g",bq="b",br="a",bs=0.349019607843137,bt="adaptiveStyles",bu="interactionMap",bv="diagram",bw="objects",bx="masters",by="objectPaths";
+return _creator();
+})());
